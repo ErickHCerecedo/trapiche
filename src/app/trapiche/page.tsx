7c3e4b72c62abe175d18c6d4a2e8be215a6aa440
Erick de Jesus Hernandez Cerecedo <erickcerecedo@gmail.com>
2025-02-27 01:51:48 -0600

    Modificacion del archivo trapiche

--- src/app/trapiche/page.tsx
+++ src/app/trapiche/page.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import Noticias from '@/app/noticias/page'
-
 function page() {
   return (
     <>
-    <Header />
-        <Noticias />
-    <Footer />
+    
     </>
   )
 }
