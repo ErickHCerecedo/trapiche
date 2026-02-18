48361d433df576504108a53880cbe80c424cf65c
Erick de Jesus Hernandez Cerecedo <erickcerecedo@gmail.com>
2026-02-18 14:52:16 -0600

    Cambio de background

--- src/app/revista/page.tsx
+++ src/app/revista/page.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const page: React.FC = () => {
     return (
-        <div className="p-0 flex flex-col items-center">
-           <iframe 
+        <div className="p-0 flex flex-col items-center bg-black">
+           <iframe
                 src="https://player.flipsnack.com?hash=OTY5RDk2OTlFOEMrZHBsNzhhMDk1ZQ=="  
-                className='w-full h-[93vh] md:h-screen p-0 m-0'
+                className='w-full h-[93vh] md:h-screen p-0 m-0 bg-black !important'
+                style={{ backgroundColor: '#0f172a !important'}}
                 allowFullScreen 
                 allow="autoplay; clipboard-read; clipboard-write"
             ></iframe>
